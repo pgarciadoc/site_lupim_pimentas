79bcad188404700eefe83455bc64df5577a04996
Pedro Garcia <142314975+pgarciadoc@users.noreply.github.com>
2025-10-21 14:23:50 -0300

    add project

---
+++ lupim_site_code/header.php
@@ -0,0 +1,12 @@
+<header class="header">
+        <nav class="nav">
+            <div class="logo_site">
+                <img src="assets/logo site lupim.png" alt="Marca Lupim Pimentas">
+            </div>
+            <ul class="nav_list">
+                <li><a href="#quem_somos">Quem somos</a></li>
+                <li><a href="#desafio">Comprar</a></li>
+                <li><a href="#contato">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
---
+++ lupim_site_code/index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Lupim Pimentas</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Abel&family=Archivo+Black&family=Assistant:wght@200..800&family=Bebas+Neue&family=Bungee+Spice&family=Liter&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Abel&family=Archivo+Black&family=Assistant:wght@200..800&family=Bebas+Neue&family=Bungee+Spice&family=Liter&display=swap');
+    </style>
+    
+</head>
+<body>
+    <div class="position-sticky fixed-top">
+        <?php
+            include 'header.php'
+        ?>
+    </div>
+    
+    <main id="content">
+        <section id="home">
+
+                <div class="container d-flex justify-content-center">
+                    <div class="row">
+                            <div class="col-sm infos_home_marca">
+                                <div class="p-2">
+                                    <img src="assets/lupim_home.png" class="img-fluid" alt="Marca Lupim">
+                                </div>
+                                <h1>Sabor marcante <br> Com muita tradição</h1>
+                                <div class="descricao_home">
+                                    <p>Feitos com paixão e tradição, os molhos de pimenta 
+                                        Lupim são preparados artesanalmente por Luizão
+                                        Cardoso em Cândido Mota-SP, trazendo o sabor
+                                        intenso e autêntico da pimenta para transformar
+                                        suas receitas. Cada frasco é um convite à aventura
+                                        gastronômica, com ingredientes selecionados e
+                                        muito cuidado no preparo, oferecendo a combinação
+                                        perfeita de sabor, ardência e qualidade.
+                                        Desperte o paladar, adicione mais sabor ao seu dia!
+                                        Ideal para todas as refeições, principalmente 
+                                        para aquele churrasco de domingo, com uma
+                                        boa costela na brasa.
+                                    </p>
+                                </div>
+                                <div class="-sm infos_home_marca">
+                                    <button>Compre Agora</button>
+                                </div>
+                            </div>    
+                    </div>
+                </div>
+
+        </section>
+
+        <section id="desafio">
+            <div class="slogan_desafio">
+                <img src="assets/slogan_desafio.png" alt="Escolha seu desafio">
+            </div>
+
+            <div class="market_place">
+
+                <div class="pimenta_1">
+                    <img src="assets/pimenta_1.png" alt="Pimenta artesanal de pimenta 1">
+                    <h2>Nome da Pimenta</h2>
+                    <button class="compre_agora_pimenta_1">Compre agora</button>
+                </div>
+    
+                <div class="pimenta_2">
+                    <img src="assets/pimenta_2.png" alt="Pimenta artesanal de pimenta 2">
+                    <h2>Nome da Pimenta</h2>
+                    <button class="compre_agora_pimenta_2">Compre agora</button>
+                </div>
+    
+                <div class="pimenta_3">
+                    <img src="assets/pimenta_3.png" alt="Pimenta artesanal de pimenta 3">
+                    <h2>Nome da Pimenta</h2>
+                    <button class="compre_agora_pimenta_3">Compre agora</button>
+                </div>
+
+            </div>
+            
+        </section>
+
+        <section id="informativo_pimenta_1">
+            <div class="vidro_pimenta_1">
+                <img src="assets/lupim_rotulo_home.png" alt="Rótulo Pimenta Lupim">
+            </div>
+
+            <div class="textos_pimenta_1">
+                <button>Nome da pimenta</button>
+
+                <p>
+                    Consagrada como a mais ardida da casa,
+                    o molho artesanal da pimenta Skorpion promete
+                    te levar às alturas. Aqui a balança pesa mais para
+                    o lado da ardência, ideal para degustadores e
+                    degustadoras casca grossa.
+                    Com um sabor marcante, esse molho irá proporcionar
+                    uma verdadeira avalanche de sensações.
+                </p>
+
+            </div>
+
+        </section>
+
+        <section id="informativo_pimenta_2">
+
+                <div class="textos_pimenta_2">
+                    <button>Nome da pimenta</button>
+    
+                    <p>
+                        Consagrada como a mais ardida da casa,
+                        o molho artesanal da pimenta Skorpion promete
+                        te levar às alturas. Aqui a balança pesa mais para
+                        o lado da ardência, ideal para degustadores e
+                        degustadoras casca grossa.
+                        Com um sabor marcante, esse molho irá proporcionar
+                        uma verdadeira avalanche de sensações.
+                    </p>
+    
+                </div>
+
+                <div class="vidro_pimenta_2">
+                    <img src="assets/lupim_rotulo_home.png" alt="Rótulo Pimenta Lupim">
+                </div>    
+        </section>
+
+        <section id="informativo_pimenta_3">
+
+            <div class="vidro_pimenta_3">
+                <img src="assets/lupim_rotulo_home.png" alt="Rótulo Pimenta Lupim">
+            </div>  
+
+                <div class="textos_pimenta_3">
+                    <button>Nome da pimenta</button>
+    
+                    <p>
+                        Consagrada como a mais ardida da casa,
+                        o molho artesanal da pimenta Skorpion promete
+                        te levar às alturas. Aqui a balança pesa mais para
+                        o lado da ardência, ideal para degustadores e
+                        degustadoras casca grossa.
+                        Com um sabor marcante, esse molho irá proporcionar
+                        uma verdadeira avalanche de sensações.
+                    </p>
+    
+                </div>
+                     
+        </section>
+
+        <section id="quem_somos">
+            <div class="titulo_quemsomos">
+                <h2>Quem somos</h2>
+            </div>
+            
+            <div class="descricao_quemsomos">
+                <p>
+                    A Lupim nasceu de uma pessoa fascinada por pimentas, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum placerat est vitae consectetur. Praesent eu metus eu leo sodales hendrerit. Nulla iaculis eu tortor non facilisis. Nulla euismod consequat luctus. Ut varius eget neque ac tempus. Praesent iaculis est et ex viverra porta. Proin erat felis, maximus eget nulla vitae, maximus viverra orci. Mauris hendrerit consequat dignissim. Donec diam neque, tempus nec metus at, bibendum fermentum libero. Pellentesque ultrices venenatis facilisis. Sed et orci id ex aliquam laoreet posuere non sem. Nunc sapien risus, semper molestie condimentum in, facilisis vitae lectus. Vestibulum fermentum congue velit eget aliquet. Aenean dignissim.   
+                </p>
+            </div>
+            
+        </section>
+
+        <section id="contato">
+            <div class="titulo_contato">
+                <button class="Contato">Entre em Contato agora</button>
+            </div>
+
+            <div class="cta">
+                <p>Quer fazer seu pedido? Você Possui alguma dúvida? <br>
+                    Entre em contato com nossa equipe:
+                </p>
+            </div>
+
+            <div class="link_direto_whats">
+
+                <a href="https://wa.me/5518997828081/">
+                    <img src="assets/whatsapp_icon.png" alt="ícone do WhatsApp">
+                </a>
+
+                <a href="https://wa.me/5518997828081/">Clique aqui para falar com a gente</a>
+            </div>
+
+        </section>
+        
+    </main>
+    
+    <footer>
+
+        <section id="rodape">
+            
+            <div class="infos_rodape">
+                <p>© Copyright 2025 - Lupim Pimentas <br>
+                    Site desenvolvido por Nebula Works
+                </p>
+            </div>
+            
+        </section>
+
+    </footer>
+
+</body>
+</html>
